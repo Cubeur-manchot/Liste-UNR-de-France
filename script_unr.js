4200c5f68c8134cecc2c77f188d056ddf01b1d06
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2017-01-12 14:15:29 +0100

    Square One + image lien youtube

--- script_unr.js
+++ script_unr.js
@@ -1,6 +1,6 @@
 function footer()
 {
-	document.write('<footer>Vous êtes sur la "version site" de la liste UNR de France. La version la plus à jour est <a href="https://docs.google.com/spreadsheets/d/1ClyK6KtsZHDJrHY8uc-1Iri3Yq4F6RX3vP4u56S7D8Y/edit#gid=0">celle-ci</a>.<br/>Il est possible que certains records ne soient pas mis à jour. Pour toute erreur, question ou suggestion, envoyer un MP à Cubeur-manchot sur le forum Francocube.</footer>');
+	document.write('<footer>La version Google Sheet est <a href="https://docs.google.com/spreadsheets/d/1ClyK6KtsZHDJrHY8uc-1Iri3Yq4F6RX3vP4u56S7D8Y/edit#gid=0">ici</a>.<br/>Pour toute erreur, question ou suggestion, <a href="http://forum.francocube.com/ucp.php?i=pm&mode=compose&u=4736">envoyer un MP à Cubeur-manchot sur le forum Francocube</a>.</footer>');
 }
 
 function script_UNR()
@@ -14,18 +14,25 @@ function script_UNR()
 			var temps = divs[0].innerHTML;
 			var nom = divs[1].innerHTML;
 			var commentaire = divs[2].innerHTML;
+			var lien_discussion = divs[3].innerHTML;
+			var lien_video = divs[4].innerHTML;
 			var html = '<div class="temps">' + temps + '</div><div class="nom">' + nom + '</div>';
 			if (commentaire != '') {
 				html += '<div class="commentaire">' + commentaire + '</div>';
 			}
-			var as = td.getElementsByTagName('a');
-			var lien_discussion = as[0].href;
-			var lien_video = as[1].href;
-			if (lien_video.substr(-1,1) != '#') { 
-				html = '<a href="' + lien_video + '" class="lien_video" target="_blank" title="Youtube">' + html + '</a>';
-			} else {
-				if (lien_discussion.substr(-1,1) != '#') { 
+			if (lien_video == '') {
+				if (lien_discussion == '') {
+					td.className = 'sans_lien';
+				} else {
 					html = '<a href="' + lien_discussion + '" class="lien_discussion" target="_blank" title="Discussion">' + html + '</a>';
+					td.className = 'avec_discussion'
+				}
+			} else {
+				html = '<a href="' + lien_video + '" class="lien_video" target="_blank" title="Youtube">' + html + '</a>';
+				if (lien_discussion == '') {
+					td.className = 'avec_video';
+				} else {
+					td.className = 'avec_video_et_discussion';
 				}
 			}
 			td.innerHTML = html;
