acfcffd337c5e34905674e16bd6ba6d7c31d0ee0
Cubeur-manchot <matthieu.aubert@etu.emse.fr>
2017-02-23 05:06:22 +0100

    Version finale avec le XML
    C'est beau...

--- script_unr.js
+++ script_unr.js
@@ -1,79 +1,85 @@
-function footer()
+function getXML()
 {
-	document.write('<footer>La version Google Sheet est <a href="https://docs.google.com/spreadsheets/d/1ClyK6KtsZHDJrHY8uc-1Iri3Yq4F6RX3vP4u56S7D8Y/edit#gid=0">ici</a>.<br/>Pour toute erreur, question ou suggestion, <a href="http://forum.francocube.com/ucp.php?i=pm&mode=compose&u=4736">envoyer un MP à Cubeur-manchot sur le forum Francocube</a>.</footer>');
-}
-
-function script_UNR()
-{
-	var tds = document.getElementsByTagName('td');
-	var ntds = tds.length;
-	for (var i = 0; i < ntds; i++) {
-		var td = tds[i];
-		if (td.className != 'nom_epreuve') {
-			var divs = td.getElementsByTagName('div');
-			var temps = divs[0].innerHTML;
-			var nom = divs[1].innerHTML;
-			var commentaire = divs[2].innerHTML;
-			var lien_discussion = divs[3].innerHTML;
-			var lien_video = divs[4].innerHTML;
-			var html = '<div class="temps">' + temps + '</div><div class="nom">' + nom + '</div>';
-			if (commentaire != '') {
-				html += '<div class="commentaire">' + commentaire + '</div>';
-			}
-			if (lien_video == '') {
-				if (lien_discussion == '') {
-					td.className = 'sans_lien';
-				} else {
-					html = '<a href="' + lien_discussion + '" class="lien_discussion" target="_blank" title="Discussion">' + html + '</a>';
-					td.className = 'avec_discussion';
-				}
-			} else {
-				if (lien_discussion == '') {
-					td.className = 'avec_video';
-				html = '<a href="' + lien_video + '" class="lien_video" target="_blank" title="Youtube">' + html + '</a>';
-				} else {
-					td.className = 'avec_video_et_discussion';
-				html = '<a href="' + lien_video + '" class="lien_video" target="_blank" title="Youtube"></a><a href="' + lien_discussion + '" class="lien_discussion" target="_blank" title="Discussion"></a>' + html;
-				}
-			}
-			td.innerHTML = html;
-		}
-	}
-	document.getElementById('plan').innerHTML = '<p class="intitule">' + document.getElementById('plan').innerHTML + '</p>';
-	document.getElementById('filtrer').innerHTML = '<p class="intitule">' + document.getElementById('filtrer').innerHTML + '</p>';
-	document.getElementById('compact_off').innerHTML = '<p class="intitule">' + document.getElementById('compact_off').innerHTML + '</p>';
-}
-
-function plan_de_page() {
-	var plan = document.getElementById('plan');
-	var plan_ouvert = document.getElementById('plan_ouvert');
-	var plan_cache = document.getElementById('plan_cache');
-	if (plan != undefined) {
-		var parties = document.getElementsByClassName('partie');
-		var nparties = parties.length;
-		var html = '<p class="intitule">Plan de Page</p>';
-		for (var i=0; i<nparties; i++) {
-			var partie = parties[i];
-			var h2 = partie.getElementsByTagName('h2')[0];
-			html += '<div class="plan_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_plan(' + i + ',-1);"><div class="bouton_interne"></div></div><a href="#' + partie.id + '" class="plan_partie_titre" onmouseup="plan_de_page();">' + h2.innerHTML + '</a><br/>';
-			var sous_parties = partie.getElementsByClassName('sous_partie');
-			var nsous_parties = sous_parties.length;
-			for (var j=0; j<nsous_parties; j++) {
-				var sous_partie = sous_parties[j];
-				var h3 = sous_partie.getElementsByTagName('h3')[0];
-				html += '<div class="plan_sous_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_plan(' + i + ',' + j + ');"><div class="bouton_interne"></div></div><a href="#' + sous_partie.id + '" class="plan_sous_partie_titre" onmouseup="plan_de_page();">' + h3.innerHTML + '</a></div><br/>';
-			}
-			html += '</div>';
-		}
-		plan.innerHTML = html;
-		plan.id = 'plan_ouvert';
-		plan.className = 'ouvert';
-	} else if (plan_ouvert != undefined) {
-		plan_ouvert.id = 'plan_cache';
-		plan_ouvert.className = 'ferme';
+	if(window.XMLHttpRequest) {
+		var xmlhttp = new XMLHttpRequest();
 	} else {
-		plan_cache.id = 'plan_ouvert';
-		plan_cache.className = 'ouvert';
+		var xmlhttp = new activeXObject('Microsoft.XMLHTTP');
+	}
+	xmlhttp.open('get','xml_unr.xml',false);
+	xmlhttp.send();
+	var xmlDoc = xmlhttp.responseXML;
+	return(xmlDoc);
+}
+
+function nouveau_script_unr()
+{
+	var xml = getXML();
+	var balise_xml = xml.getElementsByTagName('xml')[0];
+	var balise_xml_records = balise_xml.getElementsByTagName('records')[0];
+	var balise_plan_normal = balise_xml.getElementsByTagName('plan_normal')[0];
+	make_palmares(balise_xml_records);
+	make_plan(balise_plan_normal);
+	make_records(balise_plan_normal, balise_xml_records);
+}
+
+function change_compact()
+{
+	var bouton_compact_off = document.getElementById('bouton_compact_off');
+	var xml = getXML();
+	var balise_records = xml.getElementsByTagName('records')[0];
+	if (bouton_compact_off != undefined) { /* on passe en compact */
+		var xml_plan = xml.getElementsByTagName('plan_compact')[0];
+		bouton_compact_off.id = 'bouton_compact_on';
+		bouton_compact_off.innerHTML = 'Repasser en Mode Normal';
+	} else { /* on repasse en normal */
+		var bouton_compact_on = document.getElementById('bouton_compact_on');
+		var xml_plan = xml.getElementsByTagName('plan_normal')[0];
+		bouton_compact_on.id = 'bouton_compact_off';
+		bouton_compact_on.innerHTML = 'Passer en Mode Compact';
+	}
+	make_records(xml_plan, balise_records);
+	make_plan(xml_plan);
+}
+
+function make_plan(xml_plan)
+{
+	var bouton_compact_on = document.getElementById('bouton_compact_on');
+	if (bouton_compact_on != undefined) {
+		var compact = true;
+		var string_compact = ' style="display: block; padding: 5px; margin: 0px;"';
+	} else {
+		var compact = false;
+		var string_compact = ' style="display: inline-block; padding: 5px;"';
+	}
+	var html_plan = '';
+	var plan_de_page = document.getElementById('plan_de_page');
+	var sections = xml_plan.getElementsByTagName('section');
+	var n_sections = sections.length;
+	for (var i=0; i<n_sections; i++) {
+		var section = sections[i];
+		var nom_section = section.getAttribute('nom');
+		var id_section = to_id(nom_section);
+		html_plan += '<div class="plan_partie"' + string_compact + '><div class="bouton_externe" onclick="afficher_cacher_bouton_plan(' + i + ',-1);"><div class="bouton_interne"></div></div><a href="' + id_section + '" class="plan_partie_titre">' + nom_section + '</a><br/>';
+		var subsections = section.getElementsByTagName('subsection');
+		var n_subsections = subsections.length;
+		for (var j=0; j<n_subsections; j++) {
+			var subsection = subsections[j];
+			var nom_subsection = subsection.getAttribute('nom');
+			var id_subsection = to_id(nom_subsection);
+			html_plan += '<div class="plan_sous_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_plan(' + i + ',' + j + ')"><div class="bouton_interne"></div></div><a href="' + id_subsection + '" class="plan_sous_partie_titre">' + nom_subsection + '</a></div><br/>';
+		}
+	html_plan += '</div>';
+	}
+	plan_de_page.innerHTML = html_plan;
+}
+
+function afficher_cacher_plan()
+{
+	var plan_de_page = document.getElementById('plan_de_page');
+	if (plan_de_page.style.display == 'none') {
+		plan_de_page.style.display = 'inline-block';
+	} else {
+		plan_de_page.style.display = 'none';
 	}
 }
 
@@ -96,131 +102,151 @@ function afficher_cacher_bouton_plan(i,j) {
 	plan_de_page();
 }
 
-function filtrer() {
-	var filtrer = document.getElementById('filtrer');
-	var filtrer_ouvert = document.getElementById('filtrer_ouvert');
-	var filtrer_cache = document.getElementById('filtrer_cache');
-	if (filtrer != undefined) {
-		var html = '<p class="intitule">Filtrer</p>';
-		html += '<div class="filtrer_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_filtrer(1)"><div class="bouton_interne"></div></div><div class="filtrer_average">Single</div></div>';
-		html += '<div class="filtrer_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_filtrer(2)"><div class="bouton_interne"></div></div><div class="filtrer_average">Mo3</div></div>';
-		html += '<div class="filtrer_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_filtrer(3)"><div class="bouton_interne"></div></div><div class="filtrer_average">Avg5</div></div>';
-		html += '<div class="filtrer_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_filtrer(4)"><div class="bouton_interne"></div></div><div class="filtrer_average">Avg12</div></div>';
-		html += '<div class="filtrer_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_filtrer(5)"><div class="bouton_interne"></div></div><div class="filtrer_average">Avg50</div></div>';
-		html += '<div class="filtrer_partie"><div class="bouton_externe" onclick="afficher_cacher_bouton_filtrer(6)"><div class="bouton_interne"></div></div><div class="filtrer_average">Avg100</div></div>';
-		filtrer.innerHTML = html;
-		filtrer.id = 'filtrer_ouvert';
-		filtrer.className = 'ouvert';
-	} else if (filtrer_ouvert != undefined) {
-		filtrer_ouvert.id = 'filtrer_cache';
-		filtrer_ouvert.className = 'ferme';
+function make_string(balise_record)
+{
+	var html_records = '';
+	var temps = balise_record.getElementsByTagName('temps')[0].innerHTML;
+	var nom = balise_record.getElementsByTagName('nom')[0].innerHTML;
+	var commentaire = balise_record.getElementsByTagName('commentaire')[0].innerHTML;
+	var lien_discussion = balise_record.getElementsByTagName('lien_discussion')[0].innerHTML;
+	var lien_video = balise_record.getElementsByTagName('lien_video')[0].innerHTML;
+	if (commentaire != '') {
+		var balise_commentaire = '<div class="commentaire">' + commentaire + '</div>';
 	} else {
-		filtrer_cache.id = 'filtrer_ouvert';
-		filtrer_cache.className = 'ouvert';
+		var balise_commentaire = '';
 	}
+	if (lien_discussion != '') {
+		if (lien_video != '') {
+			html_records += '<td class="avec_video_et_discussion"><a class="lien_video" href="' + lien_video + '" target="_blank" title="Youtube"></a><a class="lien_discussion" href="' + lien_discussion + '" target="_blank" title="Discussion"></a><div class="temps">' + temps + '</div><div class="nom">' + nom + '</div>' + balise_commentaire + '</td>';
+		} else {
+			html_records += '<td class="avec_discussion"><a class="lien_discussion" href="' + lien_discussion + '" target="_blank" title="Discussion"><div class="temps">' + temps + '</div><div class="nom">' + nom + '</div>' + balise_commentaire + '</a></td>';
+		}
+	} else {
+		if (lien_video != '') {
+			html_records += '<td class="avec_video"><a class="lien_discussion" href="' + lien_discussion + '" target="_blank" title="Youtube"><div class="temps">' + temps + '</div><div class="nom">' + nom + '</div>' + balise_commentaire + '</a></td>';
+		} else {
+			html_records += '<td class="sans_lien"><div class="temps">' + temps + '</div><div class="nom">' + nom + '</div>' + balise_commentaire + '</td>';
+		}
+	}
+	return (html_records);
 }
 
-function afficher_cacher_bouton_filtrer(i) {
-	var lignes = document.getElementsByTagName('tr');
-	var nlignes = lignes.length;
-	var tds = document.getElementsByTagName('td');
-	var ntds = tds.length;
-	var ths = document.getElementsByTagName('th');
-	var nths = ths.length;
-	var bouton_interne = document.getElementById('filtrer_ouvert').getElementsByClassName('bouton_interne')[i-1];
-	if (lignes[0].getElementsByTagName('th')[i].style.display != 'none') {
-		for (var j=0; j*7<ntds-1; j++) {
-			tds[7*j+i].style.display = 'none';
+function make_records(xml_plan, xml_records)
+{
+	var zone_records = document.getElementById('records');
+	var html_records = '';
+	var events_records = xml_records.getElementsByTagName('event');
+	var n_events_records = events_records.length;
+	var avg = new Array('single','mo3','avg5','avg12','avg50','avg100');
+	var sections = xml_plan.getElementsByTagName('section');
+	var n_sections = sections.length;
+	for (var i=0; i<n_sections; i++) {
+		var section = sections[i];
+		var nom_section = section.getAttribute('nom');
+		html_records += '<section id="' + to_id(nom_section) + '" class="partie"><h2>' + nom_section + '</h2>';
+		var subsections = section.getElementsByTagName('subsection');
+		var n_subsections = subsections.length;
+		for (var j=0; j<n_subsections; j++) {
+			var subsection = subsections[j];
+			var nom_subsection = subsection.getAttribute('nom');
+			html_records += '<section id="' + to_id(nom_subsection) + '" class="sous_partie"><h3>' + nom_subsection + '</h3><table><tr><th>Épreuve</th><th>Single</th><th>Mo3</th><th>Avg5</th><th>Avg12</th><th>Avg50</th><th>Avg100</th></tr>';
+			var events = subsection.getElementsByTagName('event');
+			var n_events = events.length;
+			for (var k=0; k<n_events; k++) {
+				var nom_event = events[k].innerHTML;
+				var indice_correspondant = -1;
+				for (var l=0; l<n_events_records; l++) {
+					if (events_records[l].getAttribute('nom') == nom_event) {
+						indice_correspondant = l;
+					}
+				}
+				var event = events_records[indice_correspondant];
+				html_records += '<tr><td class="nom_epreuve">' + nom_event + '</td>';
+				for (var l=0; l<6; l++) {
+					var record = event.getElementsByTagName(avg[l])[0];
+					html_records += make_string(record);
+				}
+				html_records += '</tr>';
+			}
+			html_records += '</table></section>';
 		}
-		for (var j=0; j*7<nths-1; j++) {
-			ths[7*j+i].style.display = 'none';
+		if (n_subsections == 0) {
+			html_records += '<table><tr><th>Épreuve</th><th>Single</th><th>Mo3</th><th>Avg5</th><th>Avg12</th><th>Avg50</th><th>Avg100</th></tr>';
+			var events = section.getElementsByTagName('event');
+			var n_events = events.length;
+			for (var k=0; k<n_events; k++) {
+				var nom_event = events[k].innerHTML;
+				var indice_correspondant = -1;
+				for (var l=0; l<n_events_records; l++) {
+					if (events_records[l].getAttribute('nom') == nom_event) {
+						indice_correspondant = l;
+					}
+				}
+				var event = events_records[indice_correspondant];
+				html_records += '<tr><td class="nom_epreuve">' + nom_event + '</td>';
+				for (var l=0; l<6; l++) {
+					var record = event.getElementsByTagName(avg[l])[0];
+					html_records += make_string(record);
+				}
+				html_records += '</tr>';
+			}
+			html_records += '</table>';
 		}
-		bouton_interne.style.background = 'rgb(255,100,100)';
-		bouton_interne.style.transform = 'translate(22px,-2px)';
-	} else {
-		for (var j=0; j*7<ntds-1; j++) {
-			tds[7*j+i].style.display = '';
-		}
-		for (var j=0; j*7<nths-1; j++) {
-			ths[7*j+i].style.display = '';
-		}
-		bouton_interne.style.background = 'rgb(100,255,100)';
-		bouton_interne.style.transform = 'translate(0px,-2px)';
+		html_records += '</section>'
 	}
-	filtrer();
+	zone_records.innerHTML = html_records;
 }
 
-function palmares() {
-	var palmares = document.getElementById('palmares');
-	var html = '';
-	var i = 0;
-	var j = 0;
-	var count = 0;
-	var valeur_courante = '';
-	var indice_meilleur = 0;
-	var rang = 0;
-	var suffixe_rang = '';
-	var noms_balises = document.getElementsByClassName('nom');
-	var noms_normaux = new Array();
-	var noms_reduits = new Array();
-	var n_normaux = noms_balises.length;
-	var n_reduits = 0;
-	var pourcentage = 0;
-	var cumule = 0;
-	var pourcentage_string = '';
-	var total = 0;
-	var support = ['',0];
-	for (i=0; i<n_normaux; i++) { /* comptage du total d'UNRs et transfert vers noms_normaux */
-		noms_normaux[i] = noms_balises[i].innerHTML;
-		if (noms_normaux[i] != '') total++;
-	}
-	noms_normaux.sort(); /* tri alphabétique */
-	i=0;
-	for (j=0; j<n_normaux; j++) {
-		valeur_courante = noms_normaux[j];
-		count = 0;
-		while (valeur_courante == noms_normaux[j] && j < n_normaux) { /* comptage par nom */
-			count++;
-			j++;
-		}
-		noms_reduits[i] = [valeur_courante,count];
-		j--;
-		i++;
-	}
-	n_reduits = i;
-	html = '<h2>Palmarès du nombre d\'UNRs</h2><table><tr><th>Personne (' + n_reduits + ')</th><th>Nombre d\'UNRs (' + total + ')</th><th>Classement</th><th>Pourcentage des UNRs</th></tr>';
-	for (i=0; i<n_reduits; i++) { /* tri bulle */
-		indice_meilleur = i;
-		for (j=i+1; j<n_reduits; j++) {
-			if (noms_reduits[j][1] > noms_reduits[indice_meilleur][1]) {
-				indice_meilleur = j;
+function make_palmares(records)
+{
+	var events = records.getElementsByTagName('event');
+	var avg = new Array('single','mo3','avg5','avg12','avg50','avg100');
+	var n_events = events.length;
+	var noms_denombre = new Array();
+	var denombre = new Array();
+	var n_noms = 0;
+	var total_denombre = 0;
+	for (var i=0; i<n_events; i++) {
+		var event = events[i];
+		for (var h=0; h<6; h++) {
+			var event_avg = event.getElementsByTagName(avg[h])[0];
+			var nom = event_avg.getElementsByTagName('nom')[0].innerHTML;
+			if (nom != '') {
+				total_denombre += 1;
+				for (var j=0; j<n_noms; j++) { /* recherche du nom actuel dans la liste des noms déjà vus */
+					if (nom == noms_denombre[j]) {
+						denombre[j] += 1;
+						while (j>0 && denombre[j]>denombre[j-1]) { /* actualisation du tri */
+							var nom_stock = noms_denombre[j];
+							var denombre_stock = denombre[j];
+							noms_denombre[j] = noms_denombre[j-1];
+							denombre[j] = denombre[j-1];
+							noms_denombre[j-1] = nom_stock;
+							denombre[j-1] = denombre_stock;
+							j -= 1;
+						}
+						j = n_noms + 1;
+					}
+				}
+				if (j == n_noms || n_noms == 0) { /* si nom inexistant dans la liste des déjà vus, on le crée */
+					noms_denombre[n_noms] = nom;
+					denombre[n_noms] = 1;
+					n_noms += 1;
+				}
 			}
 		}
-		support = noms_reduits[i];
-		noms_reduits[i] = noms_reduits[indice_meilleur];
-		noms_reduits[indice_meilleur] = support;
-		if (i == 0 || noms_reduits[i][1] != noms_reduits[i-1][1]) {
-			rang = i;
-		}
-		pourcentage = 100 * noms_reduits[i][1] / total;
-		cumule += pourcentage;
-		pourcentage_string = pourcentage + '';
-		pourcentage_string = pourcentage_string.substring(0,4);
-		if (i == 1) suffixe_rang = 'er'; else suffixe_rang = 'ème';
-		if (i !=0) html += '<tr><td>' + noms_reduits[i][0] + '</td><td>' + noms_reduits[i][1] + '</td><td>' + rang + suffixe_rang + '</td><td>' + pourcentage_string + '%</td></tr>';
 	}
-	html += '</table>';
-	palmares.innerHTML = html;
+	var html_palmares = '<h2>Palmarès du nombre d\'UNRs</h2><table><tr><th>Personne (' + n_noms + ')</th><th>Nombre d\'UNRs (' + total_denombre + ')</th><th>Pourcentage des UNRs</th></tr>'
+	var palmares = document.getElementById('palmares');
+	for (var i=0; i<n_noms; i++) { /* écriture du html du palmarès */
+		var pourcentage = ((100*denombre[i]/total_denombre)+'').substring(0,4);
+		html_palmares += '<tr><td>' + noms_denombre[i] + '</td><td>' + denombre[i] + '</td><td>' + pourcentage + '%</td></tr>';
+	}
+	html_palmares += '</table>';
+	palmares.innerHTML = html_palmares;
 }
 
-function compact() {
-	var compact_off = document.getElementById('compact_off');
-	var compact_on = document.getElementById('compact_on');
-	if (compact_off != undefined) {
-		compact_off.innerHTML = '<p class="intitule">Mode Normal</p>';
-		compact_off.id = 'compact_on';
-	} else {
-		compact_on.innerHTML = '<p class="intitule">Mode Compact</p>';
-		compact_on.id = 'compact_off';
-	}
+function to_id(s)
+{
+	return (s.replace(' ','_').replace(' ','_').replace(' ','_').replace(' ','_').replace(' ','_').replace(' ','_'));
 }
