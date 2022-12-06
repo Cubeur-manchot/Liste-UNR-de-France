25db59fd7eda6ac008db69935598c79857ca08eb
Cubeur-manchot <matthieu.aubert17@gmail.com>
2022-12-06 14:34:02 +0100

    [Records] Mastermorphix (1)

--- data/records.js
+++ data/records.js
@@ -202,7 +202,7 @@ const records = {
 		avg100: {name: "Wilfrid Py", time: "45.30", date: "2020-02-18", discordLink: "https://discordapp.com/channels/329175643877015553/344073328744464384/679390729260367893"}
 	},
 	"Mastermorphix": {
-		single: {name: "Maxime Dotto", time: "21.19", date: "2022-10-30", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1036356089568051320"},
+		single: {name: "Maxime Dotto", time: "11.56", date: "2022-12-02", scramble: "R2 D' R2 D2 U B2 D' B L' F2 R' B F2 D F' R2 F' R'", youtubeVideoId: "p8AtvxIi0Jc", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1048296956646137987"},
 		mo3: {name: "Maxime Dotto", time: "25.59", date: "2022-09-13", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1019207803321208882"},
 		avg5: {name: "Maxime Dotto", time: "26.62", date: "2022-09-27", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1024258456972177468"},
 		avg12: {name: "Maxime Dotto", time: "28.51", date: "2022-09-27", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1024258456972177468"},
