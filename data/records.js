aa17521a76f97bbd1e041d709b13ba1297756ae5
Cubeur-manchot <matthieu.aubert17@gmail.com>
2024-07-06 18:43:30 +0200

    [Records] Kilominx (1, 3, 5, 50, 100)

--- data/records.js
+++ data/records.js
@@ -74,12 +74,12 @@ const records = {
 		avg5: {name: "Patrick Jamet", score: "75", date: "2021-03-31"}
 	},
 	"Kilominx": {
-		single: {name: "Théo Paris", time: "11.29", date: "2018-02-17"},
-		mo3: {name: "Théo Paris", time: "15.02", date: "2018-02-17", francocubePostId: "272739"},
-		avg5: {name: "Théo Paris", time: "15.26", date: "2018-02-17"},
+		single: {name: "Rémi Perrin", time: "10.59", date: "2024-05-07", scramble: "R++ D-- R++ D++ R-- D-- R++ D-- R-- D-- U' R++ D-- R-- D-- R++ D-- R-- D-- R++ D++ U R-- D++ R-- D++ R++ D-- R-- D++ R++ D++ U", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1237502691773780020"},
+		mo3: {name: "Rémi Perrin", time: "14.17", date: "2024-05-02", timeList: ["12.36", "16.95", "13.21"], discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1235640849124298874"},
+		avg5: {name: "Rémi Perrin", time: "14.69", date: "2024-05-02", timeList: ["13.90", "19.64", "12.36", "16.95", "13.21"], discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1235640849124298874"},
 		avg12: {name: "Théo Paris", time: "15.93", date: "2018-02-17"},
-		avg50: {name: "Théo Paris", time: "17.31", date: "2018-02-17"},
-		avg100: {name: "Théo Paris", time: "17.63", date: "2018-02-17"}
+		avg50: {name: "Rémi Perrin", time: "16.82", date: "2024-05-07", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1237422278720098314"},
+		avg100: {name: "Rémi Perrin", time: "17.52", date: "2024-05-07", discordLink: "https://discord.com/channels/329175643877015553/344073328744464384/1237422278720098314"}
 	},
 	"Masterkilominx": {
 		single: {name: "Axel Brisse", time: "3:22.09", date: "2020-07-10", youtubeVideoId: "4ppRIoSbNkg", discordLink: "https://discord.com/channels/329175643877015553/332432847908634644/731197865812295690"},
